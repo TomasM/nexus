3ca739702f2d5f38b175a96d04218b5574d69546
TomasM <tomas.mikelskas@gmail.com>
2013-11-19 14:56:02 +0200

    django 1.6 compat

--- nexus/sites.py
+++ nexus/sites.py
@@ -1,4 +1,5 @@
 # Core site concept heavily inspired by django.contrib.sites
+from functools import update_wrapper
 
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
@@ -7,7 +8,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils.datastructures import SortedDict
-from django.utils.functional import update_wrapper
 from django.utils.http import http_date
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
